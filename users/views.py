e07561a7d944ae911a29352186406015ba77fcef
Putin.V.V <sokorev.sod@yandex.ru>
2025-03-20 09:59:55 +0800

    com30.2

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import filters, generics, viewsets
+
+from users.models import Payment
+from users.serializers import CustomUserSerializer, PaymentSerializers
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializers
+    queryset = Payment.objects.all()
+    filter_backends = [filters.OrderingFilter]
+    filterset_fields = ["paid_course", "separately_paid_lesson", "payment_method"]
+    ordering_fields = ["payment_date"]
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = CustomUserSerializer
